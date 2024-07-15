915e5f79911374efa0027105ad820492c4ee8bc3
Wei Xiang Goh <weixianggoh1995@gmail.com>
2024-07-16 00:30:39 +0800

    feat: test

--- next-2/app/page.tsx
+++ next-2/app/page.tsx
@@ -1,11 +1,6 @@
 import Image from 'next/image'
-import { useEffect } from 'react'
 
 export default function Home() {
-    useEffect(() => {
-        console.log('Hello, World 2!')
-    }, [])
-
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
